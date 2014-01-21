6a5da815a3a6ccb85e309ce9f86db4a018777c7d
Craige McWhirter <craige@mcwhirter.com.au>
2014-01-21 22:55:38 +1100

    Completed Exercises 1 - 4.

---
+++ ex3.py
@@ -0,0 +1,34 @@
+# Print the statement
+print "I will now count my chickens:"
+
+# Calculate the number of hens
+print "Hens", 25.00 + 30.00 / 6.00
+# Caluclate the number of roosters
+print "Roosters", 100.00 - 25.00 * 3.00 % 4.00
+
+#Print out the eggs
+print "Now I will count the eggs:"
+
+#Calculate the number of eggs
+print 3.00 + 2.00 + 1.00 - 5.00 + 4.00 % 2.00 - 1.00 / 4.00 + 6.00
+
+#Print yet another statement
+print "Is it true that 3 + 2 < 5 - 7?"
+
+#Calculate the truth!
+print 3.00 + 2.00 < 5.00 -7.00
+
+#Print questions then calculate the results
+print "What is 3 + 2?", 3.00 + 2.00
+print "What is 5 - 7?", 5.00 - 7.00
+
+#Print another statement
+print "Oh, that's why it's False."
+
+#Another print example
+print "How about some more."
+
+#Print some more statements then run equations:
+print "Is it greater?", 5.00 > -2.00
+print "Is it greater or equal?", 5.00 >= -2.00
+print "Is it less or equal?", 5.00 <= -2.00
---
+++ ex3_1.py
@@ -0,0 +1,9 @@
+#This exercise is to culculate something new
+
+#Start it simple
+print "What is 2 + 2?", 2 + 2
+
+#Okay now I've mastered that, let's do something else
+print "How many 7s in 42?", 42 / 7
+
+print "that's enough."
