19a70672b78fb6c7fcbe4277e204a6fae3424f6f
Craige McWhirter <craige@mcwhirter.com.au>
2014-02-04 08:51:03 +1100

    Completed ex34

---
+++ ex34.py
@@ -0,0 +1,22 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print "The list of animals is: "
+for i in animals:
+	print i	
+
+# The animal at 1.
+print "The animal at 1 is: " + animals[1]
+# The third (3rd) animal.
+print "The third (3rd) animal is: " + animals[2]
+# The first (1st) animal.
+print "The first (1st) animal is: " + animals[0]
+# The animal at 3.
+print "The animal at 3 is: " + animals[3]
+# The fifth (5th) animal.
+print "The fifth (5th) animal is: " + animals[4]
+# The animal at 2.
+print "The animal at 2 is: " + animals[2]
+# The sixth (6th) animal.
+print "The sixth (6th) animal is: " + animals[5]
+# The animal at 4.
+print "The animal at 4 is: " + animals[4]
