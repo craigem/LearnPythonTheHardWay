4f9175f7f03e291c166d759434a2d5d2b54f0d98
Craige McWhirter <craige@mcwhirter.com.au>
2014-03-09 08:07:13 +1100

    Started ex42

---
+++ ex42.py
@@ -0,0 +1,27 @@
+## Animal is-a object (yes, sort of confusing) look at the extra credit
+class Animal(object):
+	pass
+
+## ??
+class Dog(Animal):
+
+	def __init__(self, name):
+		## ??
+		self,name = name
+
+## ??
+class Cat(Animal):
+
+	def __init__(self, name):
+	## ??
+	self.name = name
+
+## ??
+class Person(object):
+
+	def __init__(self, name):
+		## ??
+		self.name = name
+
+		## Person has-a pet of some kind
+		self.pet = None
