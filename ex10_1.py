02dd79bf6edbb2ea289ab87870e2e1d05c0d258c
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-13 11:00:28 +1100

    cleaned up ex10_1.py

--- ex10_1.py
+++ ex10_1.py
@@ -1,3 +1,4 @@
+"""Exercise 10, Study Drill 1, Learning Python the Hard Way"""
 while True:
-   for i in ["/","-","|","\\","|"]:
-       print "%s\r" % 1,
+    for i in ["/", "-", "|", "\\", "|"]:
+        print "%s\r" % 1,
