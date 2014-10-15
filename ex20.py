e7e57c2f6c3a6d010e5d145c9be72ab7263b868f
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-16 07:06:09 +1100

    Corrected issues identified by pylint

--- ex20.py
+++ ex20.py
@@ -1,33 +1,37 @@
+"""Exercise 20, Learning Python the Hard Way"""
 from sys import argv
 
-script, input_file = argv
+SCRIPT, INPUT_FILE = argv
 
 def print_all(f):
-	print f.read()
+    """Print the whole file"""
+    print f.read()
 
 def rewind(f):
-	f.seek(0)
+    """Rewind to the start of the file"""
+    f.seek(0)
 
 def print_a_line(line_count, f):
-	print line_count, f.readline()
+    """Print one of the lines"""
+    print line_count, f.readline()
 
-current_file = open(input_file)
+CURRENT_FILE = open(INPUT_FILE)
 
 print "First let's print the whole file:\n"
 
-print_all(current_file)
+print_all(CURRENT_FILE)
 
 print "Now let's rewind, kind of like a tape,"
 
-rewind(current_file)
+rewind(CURRENT_FILE)
 
 print "Let's print three lines:"
 
-current_line = 1
-print_a_line(current_line, current_file)
+CURRENT_LINE = 1
+print_a_line(CURRENT_LINE, CURRENT_FILE)
 
-current_line = current_line + 1
-print_a_line(current_line, current_file)
+CURRENT_LINE = CURRENT_LINE + 1
+print_a_line(CURRENT_LINE, CURRENT_FILE)
 
-current_line = current_line + 1
-print_a_line(current_line, current_file)
+CURRENT_LINE = CURRENT_LINE + 1
+print_a_line(CURRENT_LINE, CURRENT_FILE)
