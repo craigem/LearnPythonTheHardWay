979e467bd989ffc8468a2a2227932e24c1a8331d
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-15 21:41:23 +1100

    Corrected issues identified by pylint

--- ex13.py
+++ ex13.py
@@ -1,14 +1,15 @@
+"""Exercise 13, Learning Python the Hard Way"""
 # -- coding: utf-8 --
 
 # Import argv from sys
 from sys import argv
 
 # Set the variable to argv
-script, first, second, third = argv
+SCRIPT, FIRST, SECOND, THIRD = argv
 
 # Print them.
 
-print "The script is called:", script
-print "Your first variable is:", first
-print "Yout second variable is:", second
-print "Your third variable is:", third
+print "The script is called:", SCRIPT
+print "Your first variable is:", FIRST
+print "Yout second variable is:", SECOND
+print "Your third variable is:", THIRD
