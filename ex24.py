004693026af88807b80f109aa9527f7051cf549d
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-16 07:15:53 +1100

    Corrected issues identified by pylint

--- ex24.py
+++ ex24.py
@@ -1,7 +1,9 @@
+"""Exercise 24, Learning Python the Hard Way"""
 print "Let's practive everything."
-print 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
+print 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t \
+    tabs.'
 
-poem = """
+POEM = """
 \tThe lovely world
 with logic so firmly planted
 cannot discern \n the needs of love
@@ -11,27 +13,29 @@ and requires an explanation
 """
 
 print "--------------"
-print poem
+print POEM
 print "--------------"
 
 
-five = 10 - 2 + 3 - 6
-print "This should be five: %s" % five
+FIVE = 10 - 2 + 3 - 6
+print "This should be five: %s" % FIVE
 
 def secret_formula(started):
-	jelly_beans = started * 500
-	jars = jelly_beans / 1000
-	crates = jars / 100
-	return jelly_beans, jars, crates
+    """Secret forumula for calculating the number of jars, bean and crates"""
+    jelly_beans = started * 500
+    jars = jelly_beans / 1000
+    crates = JARS / 100
+    return jelly_beans, jars, crates
 
 
-start_point = 10000
-beans, jars, crates = secret_formula(start_point)
+START_POINT = 10000
+BEANS, JARS, CRATES = secret_formula(START_POINT)
 
-print "With a starting point of: %d" % start_point
-print "We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates)
+print "With a starting point of: %d" % START_POINT
+print "We'd have %d beans, %d jars, and %d crates." % (BEANS, JARS, CRATES)
 
-start_point = start_point / 10
+START_POINT = START_POINT / 10
 
 print "We can also do that this way:"
-print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point)
+print "We'd have %d beans, %d jars, and %d crates." % secret_formula(
+    START_POINT)
