6ef9c06cb4735a2c9ec8c890fc33fb0a7290364e
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-15 21:49:51 +1100

    Corrected issues identified by pylint

--- ex15.py
+++ ex15.py
@@ -1,21 +1,22 @@
+"""Exercise 15, Learning Python the Hard Way"""
 # Import argv from the sys module
 from sys import argv
 
 # Set the variables to be taken from argv
-script, filename = argv
+SCRIPT, FILENAME = argv
 
 # Set txt to open the filename from argv
-txt = open(filename)
+TXT = open(FILENAME)
 
 # Use txt to print the contents of the file
-print "Here's your filename %r:" % filename
-print txt.read()
+print "Here's your filename %r:" % FILENAME
+print TXT.read()
 
 # Prompt the uer to ree-type the file name:
 print "Type the filename again:"
-file_again = raw_input("> ")
+FILE_AGAIN = raw_input("> ")
 
 # Use read that file and print the file.
-txt_again = open(file_again)
+TXT_AGAIN = open(FILE_AGAIN)
 
-print txt_again.read()
+print TXT_AGAIN.read()
