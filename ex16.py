9a1484347d280caf002e96ea6b972146b720d9c5
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-15 21:55:41 +1100

    Corrected issues identified by pylint

--- ex16.py
+++ ex16.py
@@ -1,33 +1,34 @@
+"""Exercise 16, Learning Python the Hard Way"""
 from sys import argv
 
-script, filename = argv
+SCRIPT, FILENAME = argv
 
-print "we're going to erase %r." % filename
+print "we're going to erase %r." % FILENAME
 print "If you don't wasnt that, hit CTRL-C (^C)."
 print "If you do want that, hit RETURN."
 
 raw_input("?")
 
 print "Opening the file..."
-target = open(filename, 'w')
+TARGET = open(FILENAME, 'w')
 
 print "Truncating the file.  Goodbye!"
-target.truncate()
+TARGET.truncate()
 
 print "Now I'm going to ask you for three lines."
 
-line1 = raw_input("line 1: ")
-line2 = raw_input("line 2: ")
-line3 = raw_input("line 3: ")
+LINE1 = raw_input("line 1: ")
+LINE2 = raw_input("line 2: ")
+LINE3 = raw_input("line 3: ")
 
 print "I'm going to write these to the file."
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+TARGET.write(LINE1)
+TARGET.write("\n")
+TARGET.write(LINE2)
+TARGET.write("\n")
+TARGET.write(LINE3)
+TARGET.write("\n")
 
 print "And finally, we close it."
-target.close()
+TARGET.close()
