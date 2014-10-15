51e6a8a23c1dc2dabb988a04e887cb6e33058cf7
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-15 22:02:40 +1100

    Corrected issues identified by pylint

--- ex17.py
+++ ex17.py
@@ -1,24 +1,25 @@
+"""Exercise 17, Learning Python the Hard Way"""
 from sys import argv
 from os.path import exists
 
-script, from_file, to_file = argv
+SCRIPT, FROM_FILE, TO_FILE = argv
 
-print "Copyng from %s to %s" % (from_file, to_file)
+print "Copyng from %s to %s" % (FROM_FILE, TO_FILE)
 
 # we could do these two on one line too, how?
-in_file = open(from_file)
-indata = in_file.read()
+IN_FILE = open(FROM_FILE)
+INDATA = IN_FILE.read()
 
-print "THe input file is %d bytes long" % len(indata)
+print "THe input file is %d bytes long" % len(INDATA)
 
-print "Does the output file exist? %r" % exists(to_file)
+print "Does the output file exist? %r" % exists(TO_FILE)
 print "Ready, hit RETURN to continue, CTLRL-C to abort."
 raw_input()
 
-out_file = open(to_file, 'w')
-out_file.write(indata)
+OUT_FILE = open(TO_FILE, 'w')
+OUT_FILE.write(INDATA)
 
 print "Alright, all done."
 
-out_file.close()
-in_file.close()
+OUT_FILE.close()
+IN_FILE.close()
