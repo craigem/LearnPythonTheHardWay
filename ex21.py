2b1bf9d766d1b5177a6fccebca9753aac190f63d
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-16 07:06:15 +1100

    Corrected issues identified by pylint

--- ex21.py
+++ ex21.py
@@ -1,33 +1,38 @@
+"""Exercise 21, Learning Python the Hard Way"""
 def add(a, b):
-	print "ADDING %d + %d" % (a, b)
-	return a + b
+    """Print the sum of a and b"""
+    print "ADDING %d + %d" % (a, b)
+    return a + b
 
 def subtract(a, b):
-	print "SUBTRACTING %d - %d" % (a, b)
-	return a - b 
+    """Print the result of the sutraction of a from b"""
+    print "SUBTRACTING %d - %d" % (a, b)
+    return a - b
 
 def multiply(a, b):
-	print "MULTIPLYING %d * %d" % (a, b)
-	return a * b
+    """Print the result of the multiplication of a and b"""
+    print "MULTIPLYING %d * %d" % (a, b)
+    return a * b
 
 def divide(a, b):
-	print "DIVIDING %d / %d" % (a, b)
-	return a / b
+    """Print the result of the division of a by b"""
+    print "DIVIDING %d / %d" % (a, b)
+    return a / b
 
 
 print "Let's do some math with just functions!"
 
-age = add(30, 5)
-height = subtract(78, 4)
-weight = multiply(90, 2)
-iq = divide(100, 2)
+AGE = add(30, 5)
+HEIGHT = subtract(78, 4)
+WEIGHT = multiply(90, 2)
+IQ = divide(100, 2)
 
-print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq)
+print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (AGE, HEIGHT, WEIGHT, IQ)
 
 
 # A puzzle for the extra credit, type it in anyway.
-print "Here is a puzzle." 
+print "Here is a puzzle."
 
-what  =  add(age, subtract(height, multiply(weight, divide(iq, 2))))
+WHAT = add(AGE, subtract(HEIGHT, multiply(WEIGHT, divide(IQ, 2))))
 
-print "That becomes: ", what, "Can you do it by hand?"
+print "That becomes: ", WHAT, "Can you do it by hand?"
