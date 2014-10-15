36a2c3b056f36ef638f62a1402ad45e24712db71
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-15 22:48:01 +1100

    Corrected issues identified by pylint

--- ex19.py
+++ ex19.py
@@ -1,8 +1,10 @@
+"""Exercise 19, Learning Python the Hard Way"""
 def cheese_and_crackers(cheese_count, boxes_of_crackers):
-	print "You have %d cheeses!" % cheese_count
-	print "You have %d boxes of crackers!" % boxes_of_crackers
-	print "Man that's enough for a party!"
-	print "Get a blanket.\n"
+    """Print the crackers!"""
+    print "You have %d cheeses!" % cheese_count
+    print "You have %d boxes of crackers!" % boxes_of_crackers
+    print "Man that's enough for a party!"
+    print "Get a blanket.\n"
 
 
 print "We can just give the function numbers directly:"
@@ -10,10 +12,10 @@ cheese_and_crackers(20, 30)
 
 
 print "OR, we can use variables from our script:"
-amount_of_cheese = 10
-amount_of_crackers = 50
+AMOUNT_OF_CHEESE = 10
+AMOUNT_OF_CRACKERS = 50
 
-cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+cheese_and_crackers(AMOUNT_OF_CHEESE, AMOUNT_OF_CRACKERS)
 
 
 print "We can even do math inside too:"
@@ -21,4 +23,4 @@ cheese_and_crackers(10 + 20, 5 + 6)
 
 
 print "And we can combine the two, variablers and math:"
-cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
+cheese_and_crackers(AMOUNT_OF_CHEESE + 100, AMOUNT_OF_CRACKERS + 1000)
