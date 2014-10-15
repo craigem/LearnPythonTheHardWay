dbd9cf905db50a2a4b327c41012ca30721ec5b26
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-15 21:41:14 +1100

    Corrected issues identified by pylint

--- ex12.py
+++ ex12.py
@@ -1,9 +1,10 @@
+"""Exercise 12, Learning Python the Hard Way"""
 # -- coding: utf-8 --
 
 # Gather the input variables:
-age = raw_input("How old are you? ")
-height = raw_input("How tall are you? ")
-weight = raw_input("How much do you weigh? ")
+AGE = raw_input("How old are you? ")
+HEIGHT = raw_input("How tall are you? ")
+WEIGHT = raw_input("How much do you weigh? ")
 
 # Print the result:
-print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+print "So, you're %r old, %r tall and %r heavy." % (AGE, HEIGHT, WEIGHT)
