2293c491c668ca4f985c0ea8e654332dda717e2e
Craige McWhirter <craige@mcwhirter.com.au>
2014-11-19 17:46:38 +1100

    Applied pep8 changes

--- ex18.py
+++ ex18.py
@@ -1,21 +1,29 @@
 """Exercise 18, Learning Python the Hard Way"""
 # this one is like your scripts with argv
+
+
 def print_two(*args):
     """Take two arguments and print them"""
     arg1, arg2 = args
     print "arg1: %r, arg2: %r" % (arg1, arg2)
 
 # OK, that *args is actually pointless, we can just do this
+
+
 def print_two_again(arg1, arg2):
     """Take two arguments and print them"""
     print "arg1: %r, arg2: %r" % (arg1, arg2)
 
 # this just takes one argument
+
+
 def print_one(arg1):
     """Take one arguments and print it"""
     print "arg1: %r" % arg1
 
 # this one takes no arguments
+
+
 def print_none():
     """Take no arguments and print statement"""
     print "I got nothin'."
