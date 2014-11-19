1b5d2babaee339d5b5b0f239448b22f14e770d49
Craige McWhirter <craige@mcwhirter.com.au>
2014-11-19 22:38:02 +1100

    Applied pep8 changes

--- ex3_1.py
+++ ex3_1.py
@@ -1,9 +1,10 @@
-#This exercise is to culculate something new
+"""Exercise 26, Learning Python the Hard Way"""
+# This exercise is to culculate something new
 
-#Start it simple
+# Start it simple
 print "What is 2 + 2?", 2 + 2
 
-#Okay now I've mastered that, let's do something else
+# Okay now I've mastered that, let's do something else
 print "How many 7s in 42?", 42 / 7
 
 print "that's enough."
