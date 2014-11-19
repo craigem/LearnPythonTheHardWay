9b8027aee4772bf26705805738caac8de513c65a
Craige McWhirter <craige@mcwhirter.com.au>
2014-11-20 00:19:30 +1100

    Applied pep8 changes

--- ex33_1.py
+++ ex33_1.py
@@ -1,20 +1,22 @@
+"""Exercise 33, Learning Python the Hard Way"""
 i = 0
-numbers = []
+NUMBERS = []
 
-def append_numbers(n):
+
+def append_numbers(nnn):
     """This function will increment the numbers for us."""
     global i
-    while i < n:
-		print "At the top i is %d" % i
-		numbers.append(i)
+    while i < nnn:
+        print "At the top i is %d" % i
+        NUMBERS.append(i)
 
-		i = i + 1
-		print "Numbers now: ", numbers
-		print "At the bottom i is %d" % i
+        i = i + 1
+        print "Numbers now: ", NUMBERS
+        print "At the bottom i is %d" % i
 
 append_numbers(6)
 
 print "The numbers: "
 
-for num in numbers:
-	print num
+for num in NUMBERS:
+    print num
