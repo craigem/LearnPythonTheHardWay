40dbbe6b32ddf94d270ecc08872b2b196a8f5707
Craige McWhirter <craige@mcwhirter.com.au>
2014-11-19 19:07:44 +1100

    Applied pep8 changes

--- ex20_2.py
+++ ex20_2.py
@@ -1,41 +1,45 @@
+"""Exercise 20, Learning Python the Hard Way"""
 # Import arv from sys
 from sys import argv
 
-script, input_file = argv
+SCRIPT, INPUT_FILE = argv
 
-# Define the read function
-def print_all(f):
-	print f.read()
 
-# Define the seek function
-def rewind(f):
-	f.seek(0)
+def print_all(myfile):
+    """Define the read function"""
+    print myfile.read()
 
-# Define the line count function
-def print_a_line(line_count, f):
-	print line_count, f.readline()
+
+def rewind(myfile):
+    """Define the seek function"""
+    myfile.seek(0)
+
+
+def print_a_line(line_count, myfile):
+    """Define the line count function"""
+    print line_count, myfile.readline()
 
 # Open the file specified as an argument to the script
-current_file = open(input_file)
+CURRENT_FILE = open(INPUT_FILE)
 
 # Print the whol file
 print "First let's print the whole file:\n"
 
-print_all(current_file)
+print_all(CURRENT_FILE)
 
 # Use the rewind function to seek back to the start of the file
 print "Now let's rewind, kind of like a tape,"
 
-rewind(current_file)
+rewind(CURRENT_FILE)
 
 # Set the current line, print it and increment it
 print "Let's print three lines:"
 
-current_line = 1 # Seeks set line count to 0, this makes it 1
-print_a_line(current_line, current_file)
+CURRENT_FILE = 1  # Seeks set line count to 0, this makes it 1
+print_a_line(CURRENT_FILE, CURRENT_FILE)
 
-current_line = current_line + 1 # Increments line count to 2
-print_a_line(current_line, current_file)
+CURRENT_FILE = CURRENT_FILE + 1  # Increments line count to 2
+print_a_line(CURRENT_FILE, CURRENT_FILE)
 
-current_line = current_line + 1 # Increment line count to 3
-print_a_line(current_line, current_file)
+CURRENT_FILE = CURRENT_FILE + 1  # Increment line count to 3
+print_a_line(CURRENT_FILE, CURRENT_FILE)
