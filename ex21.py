b079677d38e6d7519aa87cc20c77a485c67338fe
Craige McWhirter <craige@mcwhirter.com.au>
2014-11-19 22:17:10 +1100

    Applied pep8 changes

--- ex21.py
+++ ex21.py
@@ -1,23 +1,28 @@
 """Exercise 21, Learning Python the Hard Way"""
-def add(a, b):
-    """Print the sum of a and b"""
-    print "ADDING %d + %d" % (a, b)
-    return a + b
 
-def subtract(a, b):
-    """Print the result of the sutraction of a from b"""
-    print "SUBTRACTING %d - %d" % (a, b)
-    return a - b
 
-def multiply(a, b):
-    """Print the result of the multiplication of a and b"""
-    print "MULTIPLYING %d * %d" % (a, b)
-    return a * b
+def add(aay, bee):
+    """Print the sum of aay and bee"""
+    print "ADDING %d + %d" % (aay, bee)
+    return aay + bee
 
-def divide(a, b):
-    """Print the result of the division of a by b"""
-    print "DIVIDING %d / %d" % (a, b)
-    return a / b
+
+def subtract(aay, bee):
+    """Print the result of the sutraction of aay from bee"""
+    print "SUBTRACTING %d - %d" % (aay, bee)
+    return aay - bee
+
+
+def multiply(aay, bee):
+    """Print the result of the multiplication of aay and bee"""
+    print "MULTIPLYING %d * %d" % (aay, bee)
+    return aay * bee
+
+
+def divide(aay, bee):
+    """Print the result of the division of aay by bee"""
+    print "DIVIDING %d / %d" % (aay, bee)
+    return aay / bee
 
 
 print "Let's do some math with just functions!"
