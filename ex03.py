63977533520b88275e192cbbe6312b2a8775d20f
Craige McWhirter <craige@mcwhirter.com.au>
2014-11-19 17:25:25 +1100

    Applied pep8 changes

--- ex03.py
+++ ex03.py
@@ -7,29 +7,29 @@ print "Hens", 25.00 + 30.00 / 6.00
 # Caluclate the number of roosters
 print "Roosters", 100.00 - 25.00 * 3.00 % 4.00
 
-#Print out the eggs
+# Print out the eggs
 print "Now I will count the eggs:"
 
-#Calculate the number of eggs
+# Calculate the number of eggs
 print 3.00 + 2.00 + 1.00 - 5.00 + 4.00 % 2.00 - 1.00 / 4.00 + 6.00
 
-#Print yet another statement
+# Print yet another statement
 print "Is it true that 3 + 2 < 5 - 7?"
 
-#Calculate the truth!
-print 3.00 + 2.00 < 5.00 -7.00
+# Calculate the truth!
+print 3.00 + 2.00 < 5.00 - 7.00
 
-#Print questions then calculate the results
+# Print questions then calculate the results
 print "What is 3 + 2?", 3.00 + 2.00
 print "What is 5 - 7?", 5.00 - 7.00
 
-#Print another statement
+# Print another statement
 print "Oh, that's why it's False."
 
-#Another print example
+# Another print example
 print "How about some more."
 
-#Print some more statements then run equations:
+# Print some more statements then run equations:
 print "Is it greater?", 5.00 > -2.00
 print "Is it greater or equal?", 5.00 >= -2.00
 print "Is it less or equal?", 5.00 <= -2.00
