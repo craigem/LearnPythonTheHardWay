bc36a5498bf0ee3d18a009ce4703f4125b1e26cc
Craige McWhirter <craige@mcwhirter.com.au>
2014-11-19 17:15:53 +1100

    Applied pep8 changes

--- ex01.py
+++ ex01.py
@@ -1,10 +1,10 @@
 """Exercise 1, Learning Python the Hard Way"""
-#print "Hello World!"
-#print "Hello Again"
-#print "I like typing this."
-#print "This is fun."
-#print 'Yay! Printing.'
-#print "I'd much rather you 'not'."
-#print 'I "said" do not touch this.'
-#print "Printing another line"
+# print "Hello World!"
+# print "Hello Again"
+# print "I like typing this."
+# print "This is fun."
+# print 'Yay! Printing.'
+# print "I'd much rather you 'not'."
+# print 'I "said" do not touch this.'
+# print "Printing another line"
 print "Print only this line."
