89f80266e043f69a776f1367f82e4e97cda7dc1c
Craige McWhirter <craige@mcwhirter.com.au>
2014-11-21 18:08:56 +1100

    Applied pep8 changes

--- ex33_3.py
+++ ex33_3.py
@@ -1,24 +1,27 @@
-# 3. Add another variable to the function arguments that you can pass in that lets you change the + 1 on line 8 so you can change how much it increments by.
+"""Exercise 33, Learning Python the Hard Way"""
+# 3. Add another variable to the function arguments that you can pass in that
+# lets you change the + 1 on line 8 so you can change how much it increments by
 # 4. Rewrite the script again to use this function to see what effect that has.
 
 i = 0
-increment = 1
-numbers = []
+INCREMENT = 1
+NUMBERS = []
 
-def append_numbers(n):
-	"""This function will increment the numbers for us."""
-	global i
-	while i < n:
-		print "At the top i is %d" % i
-		numbers.append(i)
 
-		i = i + increment
-		print "Numbers now: ", numbers
-		print "At the bottom i is %d" % i
+def append_numbers(nbr):
+    """This function will increment the numbers for us."""
+    global i
+    while i < nbr:
+        print "At the top i is %d" % i
+        NUMBERS.append(i)
+
+        i = i + INCREMENT
+        print "Numbers now: ", NUMBERS
+        print "At the bottom i is %d" % i
 
 append_numbers(6)
 
 print "The numbers: "
 
-for num in numbers:
-	print num
+for num in NUMBERS:
+    print num
