e30df86031f466118225e5d86c31c396d3fa0838
Craige McWhirter <craige@mcwhirter.com.au>
2014-11-21 18:16:51 +1100

    Applied pep8 changes

--- ex33_5.py
+++ ex33_5.py
@@ -1,20 +1,24 @@
-# 5. Now, write it to use for-loops and range instead. Do you need the incrementor in the middle anymore? What happens if you do not get rid of it?
+"""Exercise 33, Learning Python the Hard Way"""
+# 5. Now, write it to use for-loops and range instead. Do you need the
+# incrementation in the middle any more? What happens if you do not get rid of
+# it?
 # A: At the bottom becomes 6 instead of 5.
 
-numbers = []
+NUMBERS = []
 
-def append_numbers(n):
-	"""This function will increment the numbers for us."""
-	for i in range(0, 6):
-		print "At the top i is %d" % i
-		numbers.append(i)
 
-		print "Numbers now: ", numbers
-		print "At the bottom i is %d" % i
+def append_numbers(nbr):
+    """This function will increment the numbers for us."""
+    for i in range(0, 6):
+        print "At the top i is %d" % i
+        NUMBERS.append(i)
+
+        print "Numbers now: ", NUMBERS
+        print "At the bottom i is %d" % i
 
 append_numbers(6)
 
 print "The numbers: "
 
-for num in numbers:
-	print num
+for num in NUMBERS:
+    print num
