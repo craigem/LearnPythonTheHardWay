d86b80db9fc945c3fe8f26826766b2b3a1b8b9df
Craige McWhirter <craige@mcwhirter.com.au>
2014-11-21 18:19:38 +1100

    Applied pep8 changes

--- ex34.py
+++ ex34.py
@@ -1,23 +1,24 @@
-animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+"""Exercise 34, Learning Python the Hard Way"""
+ANIMALS = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
 
-print "The list of animals is: ", 
-for i in animals:
-	print i ,
+print "The list of animals is: ",
+for i in ANIMALS:
+    print i,
 
-print "\n" 
+print "\n"
 # The animal at 1.
-print "The animal at 1 is: " + animals[1]
+print "The animal at 1 is: " + ANIMALS[1]
 # The third (3rd) animal.
-print "The third (3rd) animal is: " + animals[2]
+print "The third (3rd) animal is: " + ANIMALS[2]
 # The first (1st) animal.
-print "The first (1st) animal is: " + animals[0]
+print "The first (1st) animal is: " + ANIMALS[0]
 # The animal at 3.
-print "The animal at 3 is: " + animals[3]
+print "The animal at 3 is: " + ANIMALS[3]
 # The fifth (5th) animal.
-print "The fifth (5th) animal is: " + animals[4]
+print "The fifth (5th) animal is: " + ANIMALS[4]
 # The animal at 2.
-print "The animal at 2 is: " + animals[2]
+print "The animal at 2 is: " + ANIMALS[2]
 # The sixth (6th) animal.
-print "The sixth (6th) animal is: " + animals[5]
+print "The sixth (6th) animal is: " + ANIMALS[5]
 # The animal at 4.
-print "The animal at 4 is: " + animals[4]
+print "The animal at 4 is: " + ANIMALS[4]
