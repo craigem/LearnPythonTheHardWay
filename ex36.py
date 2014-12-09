e96a46b33cb7620fb14c1826036381faafa1357a
Craige McWhirter <craige@mcwhirter.com.au>
2014-12-10 10:35:32 +1100

    Made pep8 compliant and corrected pylint problems.

--- ex36.py
+++ ex36.py
@@ -1,76 +1,93 @@
+"""Exercise 36, Learning Python the Hard Way"""
 # Import exit from sys so we can exit cleanly:
 from sys import exit
 
-# Open Game in a room and present options
+
 def start():
-	# You entered a dark and stormy cave.
-	print "You have entered a dark and stormy cave..."
-	# There is a passage to the left one to the right.
-	print "There is a passage to the left and on your right."
+    """Open Game in a room and present options"""
+    # You entered a dark and stormy cave.
+    print "You have entered a dark and stormy cave..."
+    # There is a passage to the left one to the right.
+    print "There is a passage to the left and on your right."
 
-	next = raw_input("Choose your own adventure: ")
+    mynext = raw_input("Choose your own adventure: ")
 
-	if next == "left":
-	# Left will lead to hamster_room()
-		hamster_room()
-	elif next == "right":
-	# Right will lead to gerbil_room()
-		gerbil_room()
-	else:
-	# The floor opens up underneath you as Cthulu rises and swallows you whole.
-		dead("The floor opens up underneath you as Cthulu rises and swallows you whole.")
+    if mynext == "left":
+        # Left will lead to hamster_room()
+        hamster_room()
+    elif mynext == "right":
+        # Right will lead to gerbil_room()
+        gerbil_room()
+    else:
+        # The floor opens up underneath you as Cthulhu rises and swallows
+        # you whole.
+        dead("The floor opens up underneath you as Cthulhu rises and swallows "
+             " you whole.")
 
 
 # Open the hamster_room and let them know what's in it:
 def hamster_room():
-	print "\n"
-	# there's a hamster here.
-	print "There's a hamster here."
-	# a toothpick
-	# a salad sandwich
-	print "There's also a salad sandwich and a toothpick."
+    """Let's define the hamster room."""
+    print "\n"
+    # there's a hamster here.
+    print "There's a hamster here."
+    # a toothpick
+    # a salad sandwich
+    print "There's also a salad sandwich and a toothpick."
 
-	next = raw_input("What you talkin 'bout Wilis?: ")
+    mynext = raw_input("What you talkin 'bout Wilis?: ")
 
-	if next == "salad sandwich":
-	# Choosing the salad sandwich will result in the hamster devouring you
-		print "The hamster roars, leaps toward you, tears your head off and commences devouring you from the neck down."
-		exit(0)
-	elif next == "toothpick":
-	# Choosing the  toothpick will result in hmaster slaughter
-		print "You pick up the toothpick with a forward summersault before stabbing the evil hamster through the heart."
-		exit(0)
-	else:
-	# Choosing an invalid option results in death
-		dead("The floor opens up underneath you as Cthulu rises and swallows you whole.")
+    if mynext == "salad sandwich":
+        # Choosing the salad sandwich will result in the hamster devouring you
+        print "The hamster roars, leaps toward you, tears your head off and " \
+              "commences devouring you from the neck down."
+        exit(0)
+    elif mynext == "toothpick":
+        # Choosing the  toothpick will result in hamster slaughter
+        print "You pick up the toothpick with a forward somersault before " \
+              "stabbing the evil hamster through the heart."
+        exit(0)
+    else:
+        # Choosing an invalid option results in death
+        dead("The floor opens up underneath you as Cthulhu rises and "
+             "swallows you whole.")
 
 # dead option
 
-# Open the room and let them know what's in it:
+
 def gerbil_room():
-	# a gerbil
-	print "There's a gerbil here."
-	# some dental floss
-	print "There's some dental floss..."
-	# a stick of celery
-	print "...and a stick of celery."
+    """Open the room and let them know what's in it:"""
+    # a gerbil
+    print "There's a gerbil here."
+    # some dental floss
+    print "There's some dental floss..."
+    # a stick of celery
+    print "...and a stick of celery."
 
-	next = raw_input("Which do you choose?: ")
+    mynext = raw_input("Which do you choose?: ")
 
-	if next == "dental floss":
-	# Choosing the dental floss will result in the gerbil assasination.
-		print "You cartwheel across to the gerbil, collecting the dental floss along the way before strangling the evil little bugger from behind."
-		exit(0)
-	elif next == "celery":
-	# Choosing celery will result in the gerbil get a free meal of human flesh.
-		print "The gerbil lets out a blood curdling cry before leaping across the room and locking it's powerful jaw around your left big toe as it commences to devour you from the bottom up."
-		exit(0)
-	else:
-	# Choosing an invalid option results in death
-		dead("The floor opens up underneath you as Cthulu rises and swallows you whole.")
+    if mynext == "dental floss":
+        # Choosing the dental floss will result in the gerbil assassination.
+        print "You cartwheel across to the gerbil, collecting the dental " \
+              "floss along the way before strangling the evil little " \
+              "bugger from behind."
+        exit(0)
+    elif mynext == "celery":
+        # Choosing celery will result in the gerbil get a free
+        # meal of human flesh.
+        print "The gerbil lets out a blood curdling cry before leaping " \
+              "across the room and locking it's powerful jaw around your " \
+              "left big toe as it commences to devour you from the bottom up."
+        exit(0)
+    else:
+        # Choosing an invalid option results in death
+        dead("The floor opens up underneath you as Cthulhu rises and "
+             "swallows you whole.")
+
 
 def dead(why):
-	print why, "Good job!"
-	exit(0)
+    """Define death."""
+    print why, "Good job!"
+    exit(0)
 
 start()
