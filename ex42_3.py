5223f66e12ad3ee662e6b6ddf0b8f8a4473650d7
Craige McWhirter <craige@mcwhirter.com.au>
2014-12-10 22:06:02 +1100

    Made pep8 compliant and corrected pylint problems in ex42_3.py.

--- ex42_3.py
+++ ex42_3.py
@@ -1,93 +1,96 @@
-## Animal is-a object (yes, sort of confusing) look at the extra credit
+"""Exercise 42.3, Learning Python the Hard Way"""
+
+
 class Animal(object):
-	print "Altogether Animals!"
-	print "=" * 19
+    """Animal is-a object (yes, sort of confusing) look @ the extra credit."""
+    print "Altogether Animals!"
+    print "=" * 19
 
-## Dog is-a Animal
+
 class Dog(Animal):
+    """Dog is-a Animal"""
+    def __init__(self, name):
+        # ??
+        self.name = name
+        print "%s says: Woof!" % self.name
 
-	def __init__(self, name):
-		## ??
-		self.name = name
-		print "%s says: Woof!" % self.name
 
-## Cat is-a Animal
 class Cat(Animal):
+    """Cat is-a Animal"""
+    def __init__(self, name):
+        # ??
+        self.name = name
+        print "%s says: Meow!" % self.name
 
-	def __init__(self, name):
-		## ??
-		self.name = name
-		print "%s says: Meow!" % self.name
 
-## Person is-a object
 class Person(object):
+    """Person is-a object"""
+    def __init__(self, name):
+        # ??
+        self.name = name
+        print "%s says: Hello!" % self.name
 
-	def __init__(self, name):
-		## ??
-		self.name = name
-		print "%s says: Hello!" % self.name
+        # Person has-a pet of some kind
+        self.pet = None
+        print "My name is %s and this is my pet, %s." % (self.name, self.pet)
 
-		## Person has-a pet of some kind
-		self.pet = None
-		print "My name is %s and this is my pet, %s." % (self.name, self.pet)
 
-## Employee is-a Person
 class Employee(Person):
+    """Employee is-a Person"""
+    def __init__(self, name, salary):
+        # ?? hmm what is this strange magic?
+        super(Employee, self).__init__(name)
+        # ??
+        self.salary = salary
 
-	def __init__(self, name, salary):
-		## ?? hmm what is this strange magic?
-		super(Employee, self).__init__(name)
-		## ??
-		self.salary = salary
 
-## Fish is-a object
 class Fish(object):
+    """Fish is-a object"""
+    def __init__(self, name):
+        # ??
+        self.name = name
+        print "I'm %s the fish!" % self.name
 
-	def __init__(self, name):
-		## ??
-		self.name = name
-		print "I'm %s the fish!" % self.name
 
-## Salmon is-a fish
 class Salmon(Fish):
+    """Salmon is-a fish"""
+    def __init__(self, name):
+        # ??
+        self.name = name
+        print "I'm %s the salmon!" % self.name
 
-	def __init__(self, name):
-		## ??
-		self.name = name
-		print "I'm %s the salmon!" % self.name
 
-## Halibut is-a fish
 class Halibut(Fish):
+    """Halibut is-a fish"""
+    def __init__(self, name):
+        # ??
+        self.name = name
+        print "I'm %s the halibut!" % self.name
 
-	def __init__(self, name):
-		## ??
-		self.name = name
-		print "I'm %s the halibut!" % self.name
 
+# rover is-a Dog
+ROVER = Dog("Rover")
 
-## rover is-a Dog
-rover = Dog("Rover")
+# Satan is-a Cat
+SATAN = Cat("Satan")
 
-## Satan is-a Cat
-satan = Cat("Satan")	
+# mary is-a Person
+MARY = Person("Mary")
 
-## mary is-a Person
-mary = Person("Mary")
+# mary's pet is-a satan
+MARY.pet = SATAN
 
-## mary's pet is-a satan
-mary.pet = satan
+# frank is-a Employee
+FRANK = Employee("Frank", 120000)
 
-## frank is-a Employee
-frank = Employee("Frank", 120000)
+# frank's pet is-a rover
+FRANK.pet = ROVER
 
-## frank's pet is-a rover
-frank.pet = rover
+# flipper is-a fish
+FLIPPER = Fish("Flipper")
 
-## flipper is-a fish
-flipper = Fish("flipper")
+# crouse is-a Salmon
+CROUSE = Salmon("Crouse")
 
-## crouse is-a Salmon
-crouse = Salmon("Crouse")
-
-## harry is-a Halibut
-harry = Halibut("Harry")
+# harry is-a Halibut
+HARRY = Halibut("Harry")
