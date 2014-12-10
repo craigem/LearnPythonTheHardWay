c7a506ce19e8bfa8ea750164a0cb80a01e057527
Craige McWhirter <craige@mcwhirter.com.au>
2014-12-10 20:05:42 +1100

    Made pep8 compliant and corrected pylint problems.

--- ex39.py
+++ ex39.py
@@ -1,61 +1,62 @@
+"""Exercise 39, Learning Python the Hard Way"""
 # create a mapping of state to abbreviation
-states = {
-	'Oregon': 'OR',
-	'Florida': 'FL',
-	'California': 'CA',
-	'New York': 'NY',
-	'Michigan': 'MI'
+STATES = {
+    'Oregon': 'OR',
+    'Florida': 'FL',
+    'California': 'CA',
+    'New York': 'NY',
+    'Michigan': 'MI'
 }
 
 # create a basic set of states and some cities in them
-cities = {
-	'CA': 'San Francisco',
-	'MI': 'Detroit',
-	'FL': 'Jacksonville'
+CITIES = {
+    'CA': 'San Francisco',
+    'MI': 'Detroit',
+    'FL': 'Jacksonville'
 }
 
 # add some more cities
-cities['NY'] = 'New York'
-cities['OR'] = 'Portland'
+CITIES['NY'] = 'New York'
+CITIES['OR'] = 'Portland'
 
 # print out some cities
 print '_' * 10
-print "NY State has: ", cities['NY']
-print "OR State has: ", cities['OR']
+print "NY State has: ", CITIES['NY']
+print "OR State has: ", CITIES['OR']
 
 # print some states
 print '_' * 10
-print "Michigan's abbreviation is: ", states['Michigan']
-print "Florida's abbreviation is: ", states['Florida']
+print "Michigan's abbreviation is: ", STATES['Michigan']
+print "Florida's abbreviation is: ", STATES['Florida']
 
 # do it by using the state then cities dict
 print '_' * 10
-print "Michigan has: ", cities[states['Michigan']]
-print "Florida has: ", cities[states['Florida']]
+print "Michigan has: ", CITIES[STATES['Michigan']]
+print "Florida has: ", CITIES[STATES['Florida']]
 
 # print every state abbreviation
 print '_' * 10
-for state, abbrev in states.items():
-	print "%s is abbreviated %s" % (state, abbrev)
+for STATE, abbrev in STATES.items():
+    print "%s is abbreviated %s" % (STATE, abbrev)
 
 # print every city in state
 print '_' * 10
-for abbrev, city in cities.items():
-	print "%s has the city %s" % (abbrev, city)
+for abbrev, CITY in CITIES.items():
+    print "%s has the city %s" % (abbrev, CITY)
 
 # now do both at the same time
 print '_' * 10
-for state, abbrev in states.items():
-	print "%s state is abbreviated %s and has city %s" % (
-		state, abbrev, cities[abbrev])
+for STATE, abbrev in STATES.items():
+    print "%s state is abbreviated %s and has city %s" % (
+        STATE, abbrev, CITIES[abbrev])
 
 print '_' * 10
 # safely get an abbreviation by state that might not be there
-state = states.get('Texas', None)
+STATE = STATES.get('Texas', None)
 
-if not state:
-	print "Sorry, no Texas."
+if not STATE:
+    print "Sorry, no Texas."
 
 # get a city with a default value
-city = cities.get('TX', 'Does Not Exist')
-print "The city for the state of 'TX' is: %s" % city
+CITY = CITIES.get('TX', 'Does Not Exist')
+print "The city for the state of 'TX' is: %s" % CITY
