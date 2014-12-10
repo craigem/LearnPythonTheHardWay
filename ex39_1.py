d88371653a89594fe607fcd8b833db6b8db71000
Craige McWhirter <craige@mcwhirter.com.au>
2014-12-10 20:10:34 +1100

    Made pep8 compliant and corrected pylint problems.

--- ex39_1.py
+++ ex39_1.py
@@ -1,67 +1,68 @@
+"""Exercise 39, Learning Python the Hard Way"""
 # create a mapping of states / territories to abbreviation
-states = {
-	'New South Wales': 'NSW',
-	'Victoria': 'Vic',
-	'Tasmania': 'Tas',
-	'South Australia': 'SA',
-	'Western Australia': 'WA',
-	'Queensland': 'Qld',
-	'Australian Capital Territory': 'ACT',
-	'Northern Territory': 'NT'
+STATES = {
+    'New South Wales': 'NSW',
+    'Victoria': 'Vic',
+    'Tasmania': 'Tas',
+    'South Australia': 'SA',
+    'Western Australia': 'WA',
+    'Queensland': 'Qld',
+    'Australian Capital Territory': 'ACT',
+    'Northern Territory': 'NT'
 }
 
 # create a basic set of states and some cities in them
-cities = {
-	'NSW': 'Dubbo',
-	'Vic': 'Ballarat',
-	'Tas': 'Geeveston',
-	'SA': 'Adelaide',
-	'WA': 'Margaret River',
-	'Qld': 'Cairns'
+CITIES = {
+    'NSW': 'Dubbo',
+    'Vic': 'Ballarat',
+    'Tas': 'Geeveston',
+    'SA': 'Adelaide',
+    'WA': 'Margaret River',
+    'Qld': 'Cairns'
 }
 
 # add some more cities
-cities['NT'] = 'Alice Springs'
-cities['ACT'] = 'Fishwyck'
+CITIES['NT'] = 'Alice Springs'
+CITIES['ACT'] = 'Fishwyck'
 
 # print out some cities
 print '_' * 10
-print "Tas State has: ", cities['Tas']
-print "Qld State has: ", cities['Qld']
+print "Tas State has: ", CITIES['Tas']
+print "Qld State has: ", CITIES['Qld']
 
 # print some states
 print '_' * 10
-print "Western Australia's abbreviation is: ", states['Western Australia']
-print "Victoria's abbreviation is: ", states['Victoria']
+print "Western Australia's abbreviation is: ", STATES['Western Australia']
+print "Victoria's abbreviation is: ", STATES['Victoria']
 
 # do it by using the state then cities dict
 print '_' * 10
-print "New South Wales has: ", cities[states['New South Wales']]
-print "Northern Territory has: ", cities[states['Northern Territory']]
+print "New South Wales has: ", CITIES[STATES['New South Wales']]
+print "Northern Territory has: ", CITIES[STATES['Northern Territory']]
 
 # print every state abbreviation
 print '_' * 10
-for state, abbrev in states.items():
-	print "%s is abbreviated %s" % (state, abbrev)
+for STATE, abbrev in STATES.items():
+    print "%s is abbreviated %s" % (STATE, abbrev)
 
 # print every city in state
 print '_' * 10
-for abbrev, city in cities.items():
-	print "%s has the city %s" % (abbrev, city)
+for abbrev, CITY in CITIES.items():
+    print "%s has the city %s" % (abbrev, CITY)
 
 # now do both at the same time
 print '_' * 10
-for state, abbrev in states.items():
-	print "%s state is abbreviated %s and has city %s" % (
-		state, abbrev, cities[abbrev])
+for STATE, abbrev in STATES.items():
+    print "%s state is abbreviated %s and has city %s" % (
+        STATE, abbrev, CITIES[abbrev])
 
 print '_' * 10
 # safely get an abbreviation by state that might not be there
-state = states.get('Inebriated', None)
+STATE = STATES.get('Inebriated', None)
 
-if not state:
-	print "Sorry, no Inebriation."
+if not STATE:
+    print "Sorry, no Inebriation."
 
 # get a city with a default value
-city = cities.get('IN', 'Does Not Exist')
-print "The city for the state of 'IN' is: %s" % city
+CITY = CITIES.get('IN', 'Does Not Exist')
+print "The city for the state of 'IN' is: %s" % CITY
