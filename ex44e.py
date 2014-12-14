46649b4e07319cb1380f08344de17f74fe45e5a7
Craige McWhirter <craige@mcwhirter.com.au>
2014-12-15 10:32:33 +1100

    Made pep8 compliant and corrected pylint problems in ex44e.py.

--- ex44e.py
+++ ex44e.py
@@ -1,32 +1,42 @@
+"""Exercise 44, Learning Python the Hard Way"""
+
+
 class Other(object):
+    """The class Other is-a object"""
+    def override(self):
+        """Define the override method."""
+        print "OTHER override()"
 
-	def override(self):
-		print "OTHER overrride()"
+    def implicit(self):
+        """Define the implicit method."""
+        print "OTHER implicit"
 
-	def implicit(self):
-		print "OTHER implicit"
+    def altered(self):
+        """Define the altered method."""
+        print "OTHER altered()"
 
-	def altered(self):
-		print "OTHER altered()"
 
 class Child(object):
+    """The class Child is-a object."""
+    def __init__(self):
+        self.other = Other()
 
-	def __init__(self):
-		self.other = Other()
+    def implicit(self):
+        """Define the implicit method."""
+        self.other.implicit()
 
-	def implicit(self):
-		self.other.implicit()
+    def override(self):
+        """Define the override method."""
+        print "CHILD override()"
 
-	def override(self):
-		print "CHILD override()"
+    def altered(self):
+        """Define the altered method."""
+        print "CHILD, BEFORE OTHER altered()"
+        self.other.altered()
+        print "CHILD, AFTER OTHER altered()"
 
-	def altered(self):
-		print "CHILD, BEFORE OTHER altered()"
-		self.other.altered()
-		print "CHILD, AFTER OTHER altered()"
+SON = Child()
 
-son = Child()
-
-son.implicit()
-son.override()
-son.altered()
+SON.implicit()
+SON.override()
+SON.altered()
