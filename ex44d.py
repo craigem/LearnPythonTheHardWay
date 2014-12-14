bd8dbd27bd21935b652dff8b97e3a3c299262df5
Craige McWhirter <craige@mcwhirter.com.au>
2014-12-15 08:42:20 +1100

    Corrected spelling mistackes

--- ex44d.py
+++ ex44d.py
@@ -5,24 +5,24 @@ class Parent(object):
     """Opens the class Parent."""
     def override(self):
         """define the override."""
-        print "PARENT overrride()"
+        print "PARENT override()"
 
     def implicit(self):
         """define implicit inheritance"""
         print "PARENT implicit"
 
     def altered(self):
-        """define the altered inheretance"""
+        """define the altered inheritance"""
         print "PARENT altered()"
 
 
 class Child(Parent):
-    """opens the class Child with inhereitance from Parent"""
+    """opens the class Child with inheritance from Parent"""
     # define the override
     def override(self):
         print "CHILD override()"
 
-    # define the altered inheretance
+    # define the altered inheritance
     def altered(self):
         print "CHILD, BEFORE PARENT altered()"
         # Use super to call the altered Parent version
