d07850d3a76abc6f39983b5f7f0d55a9e0e5825d
Craige McWhirter <craige@mcwhirter.com.au>
2014-12-15 08:34:09 +1100

    Made pep8 compliant and corrected pylint problems in ex44c.py.

--- ex44c.py
+++ ex44c.py
@@ -1,17 +1,23 @@
-class Parent(object):
+"""Exercise 43, Learning Python the Hard Way"""
 
-	def altered(self):
-		print "PARENT altered()"
+
+class Parent(object):
+    """The Parent class is-a object."""
+    def altered(self):
+        """Define the altered method."""
+        print "PARENT altered()"
+
 
 class Child(Parent):
+    """The Child class is-a child of the Parent class."""
+    def altered(self):
+        """Define the altered method."""
+        print "CHILD, BEFORE PARENT altered()"
+        super(Child, self).altered()
+        print "CHILD, AFTER PARENT altered()"
 
-	def altered(self):
-		print "CHILD, BEFORE PARENT altered()"
-		super(Child, self).altered()
-		print "CHILD, AFTER PARENT altered()"
+DAD = Parent()
+SON = Child()
 
-dad = Parent()
-son = Child()
-
-dad.altered()
-son.altered()
+DAD.altered()
+SON.altered()
