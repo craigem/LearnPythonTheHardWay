2caaf2b10e29410d074aa997826db2b5fe757464
Craige McWhirter <craige@mcwhirter.com.au>
2014-12-25 18:22:39 +1100

    Completed pep8 and pylints changes for ex46

--- ex46/skeleton/setup.py
+++ ex46/skeleton/setup.py
@@ -1,19 +1,19 @@
 try:
-	from setuptools import setup
+    from setuptools import setup
 except ImportError
-	from distutils.core import setup
+from distutils.core import setup
 
 config = {
-	'description': 'My Project',
-	'author': 'Craige McWhirter',
-	'url': 'URL to get at it',
-	'download_url': 'Where to download it.',
-	'author_email': 'craige@mcwhirter.com.au',
-	'version': '0.1',
-	'install_requires': ['nose'],
-	'packages': ['NAME'],
-	'scripts': [],
-	'name': 'projectname'
+    'description': 'My Project',
+    'author': 'Craige McWhirter',
+    'url': 'URL to get at it',
+    'download_url': 'Where to download it.',
+    'author_email': 'craige@mcwhirter.com.au',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'projectname'
 }
 
 setup(**config)
---
+++ ex46/skeleton/tests/NAMEtests.py
@@ -0,0 +1,18 @@
+"""Simple skeleton files for tests"""
+from nose.tools import *
+import NAME
+
+
+def setup():
+    """Setup the test"""
+    print "SETUP!"
+
+
+def teardown():
+    """tear down the test"""
+    print "TEAR DOWN"
+
+
+def test_basic():
+    """Basic test function"""
+    print "I RAN!"
